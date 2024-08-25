27d5b121231c5b3a96f91127e756133ff1b0397d
JDCA <jdcastroa1927@micorreo.upp.edu.mx>
2024-08-25 03:38:41 -0600

    Update Post.jsx

--- HolaMundo/src/components/Post.jsx
+++ HolaMundo/src/components/Post.jsx
@@ -16,28 +16,35 @@ const Post = ({ title, content, imageUrl }) => {
         setComments([...comments, comment]);
     }
     };
-
+    //Renderización del componente Post
     return (
     <div style={styles.postContainer}>
+        {/*Renderiza la imagen si imageURL está presente*/}
         {imageUrl && (
         <div style={styles.imageContainer}>
             <img src={imageUrl} alt="Post" style={styles.image} />
         </div>
         )}
+        {/*Renderiza el título del post*/}
         <div style={styles.title}>
             <h2 >{title}</h2>
         </div>
+        {/*Renderiza el contenido del post*/}
         <div style={styles.content}> 
             <p>{content}</p>
         </div>
+        {/*Botón para dar like y mostrar la cantidad de likes actuales*/}
         <button onClick={handleLike} style={styles.button}>
         👍 Like ({likes})
         </button>
+        {/*Botón para agregar comentario y mostrar la cantidad de comentarios actuales*/}
         <button onClick={handleAddComment} style={styles.button}>
         💬 Comentarios ({comments.length})
         </button>
+        {/*Renderiza los comentarios si hay alguno*/}
         {comments.length > 0 && (
         <div style={styles.commentsContainer}>
+            {/* Mapea cada comentario en el array comments y lo renderiza como un párrafo */}
             {comments.map((comment, index) => (
             <p key={index} style={styles.comment}>{comment}</p>
             ))}
@@ -47,10 +54,12 @@ const Post = ({ title, content, imageUrl }) => {
     );
 };
 
+// Definición de los tipos y requisitos de las props que el componente Post espera recibir
 Post.propTypes = {
-    title: PropTypes.string.isRequired,
-    content: PropTypes.string.isRequired,
-    imageUrl: PropTypes.string,
+    title: PropTypes.string.isRequired, // title debe ser una cadena y es obligatorio
+    content: PropTypes.string.isRequired, // content debe ser una cadena y es obligatorio
+    imageUrl: PropTypes.string, // imageUrl debe ser una cadena, pero es opcional
 };
 
+// Exporta el componente Post para que pueda ser utilizado en otros archivos
 export default Post;
